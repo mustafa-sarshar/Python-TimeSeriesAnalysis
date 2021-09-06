627c96225a664a207cd1e422960f571d4de659f4
Mustafa Sarshar <mustafa.sarshar88@gmail.com>
2021-09-06 10:48:22 +0200

    update

--- 01_Basics/box_cox.py
+++ 01_Basics/box_cox.py
@@ -1,7 +1,11 @@
+"""
+Time Series Transformation by using Boxcox
+"""
 # In[] Libs
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 from scipy.stats import boxcox
 
 # In[] Datasets
@@ -10,11 +14,25 @@ _dataset_file = "01_LeftAnkle.csv"
 df = pd.read_csv(f"..\\Datasets\\{_dataset_dir}\\{_dataset_file}", skiprows=12)
 df.drop(columns=["PacketCounter", "StatusWord", "ClippingFlags", "RSSI"], inplace=True)
 
-# In[] Plot the data
+# In[] Plot the dataset
 _cols_to_plot = ["Acc_X", "Acc_Y", "Acc_Z", "Gyr_X", "Gyr_Y", "Gyr_Z"]
 plt.clf()
 plt.plot(df[_cols_to_plot])
 plt.legend(_cols_to_plot)
 plt.show()
 
-# In[] 
+# In[] Data Analysis
+scaler = MinMaxScaler(feature_range=(0.01, 1.01)) # Data must contain only positive values, therefore we scale the data from 0.01 to 1.01.
+_cols_to_workon = "Gyr_Z"
+df[_cols_to_workon+"_scaled"] = scaler.fit_transform(X=df[_cols_to_workon].values.reshape(-1, 1)) 
+data, lam = boxcox(df[_cols_to_workon+"_scaled"]) 
+df[_cols_to_workon+"_boxcox"] = data
+
+# In[] Plot the Boxcox
+_cols_to_plot = [_cols_to_workon+"_scaled", _cols_to_workon+"_boxcox"]
+plt.clf()
+plt.plot(df[_cols_to_plot])
+plt.legend(_cols_to_plot)
+plt.show()
+
+# In[]
