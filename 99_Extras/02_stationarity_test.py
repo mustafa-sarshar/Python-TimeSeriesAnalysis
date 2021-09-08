6e10e0be27f1f5df0d0ba7177fd1b804e83f78dd
Sarshar <sarshar@ads.uni-magdeburg.de>
2021-09-08 21:23:48 +0200

    update

--- 99_Extras/02_stationarity_test.py
+++ 99_Extras/02_stationarity_test.py
@@ -1,3 +1,33 @@
 """
 Augmented Dickey-Fuller Test (ADF Test)
 """
+# In[] Libs
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import chirp
+from statsmodels.tsa.stattools import adfuller
+
+# In[] define custom methods
+def adf(data, alpha_level):
+    res = adfuller(data)
+    print(f"Test-Statistic: {res[0]:0.2f}")
+    print(f"P-Value: {res[1]:0.2f}")
+    if res[1] < alpha_level:
+        print("Stationary")
+    else:
+        print("Non-Stationary")
+
+# In[] Generate some random datasets
+n_samples = 200
+
+data_stationary_1 = np.random.normal(loc=0, scale=10, size=n_samples)
+plt.plot(data_stationary_1, label="Normal distribution")
+adf(data=data_stationary_1, alpha_level=0.05)
+
+data_stationary_2 = np.random.gamma(shape=1, scale=1, size=n_samples)
+plt.plot(data_stationary_2, label="Gamma distribution")
+adf(data=data_stationary_2, alpha_level=0.05)
+
+plt.legend()
+plt.show()
